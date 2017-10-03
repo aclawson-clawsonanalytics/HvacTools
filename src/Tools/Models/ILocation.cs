75448161d082aba8b4ca0aa1d116f5ecb7eb78a7
andrewjclawso <andrew.clawson@clawsonanalytics.com>
2017-10-03 01:28:16 -0600

    add models folder.

---
+++ src/Tools/Models/ILocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools.Models
+{
+    public interface ILocation
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+}
