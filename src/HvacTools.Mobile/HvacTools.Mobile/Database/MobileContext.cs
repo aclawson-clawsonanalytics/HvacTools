cdacf225d3b2bc5d52fadf39b9698f73a6281b6a
andrewjclawso <andrew.clawson@clawsonanalytics.com>
2017-10-04 07:48:06 -0600

    progress. working on abstracting dbcontext to an interface located in the class library and building the mobile implementation of the models.

--- src/HvacTools.Mobile/HvacTools.Mobile/Database/MobileContext.cs
+++ src/HvacTools.Mobile/HvacTools.Mobile/Database/MobileContext.cs
@@ -10,6 +10,29 @@ namespace HvacTools.Mobile.Database
 {
     public class MobileContext : DbContext
     {
+        #region Member Variables
+        private string databasePath { get; set; }
         public DbSet<ILocation> Locations { get; set; }
+        public DbSet<IServiceRecord> ServiceRecords { get; set; }
+
+        #endregion
+
+        #region Constructor(s)
+        public MobileContext(string sqliteDbPath)
+        {
+            databasePath = sqliteDbPath;
+        }
+        #endregion
+
+        #region Class Methods
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+        #endregion
+
+
     }
+
+    
 }
