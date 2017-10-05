aca77ae39f111776c8c2b6cb252cac25aaa63728
andrewjclawso <andrew.clawson@clawsonanalytics.com>
2017-10-04 21:36:39 -0600

    Fill out MobileContext and add class for reference context.

--- src/HvacTools.Mobile/HvacTools.Mobile/Database/MobileContext.cs
+++ src/HvacTools.Mobile/HvacTools.Mobile/Database/MobileContext.cs
@@ -11,23 +11,24 @@ namespace HvacTools.Mobile.Database
     public class MobileContext : DbContext
     {
         #region Member Variables
-        private string databasePath { get; set; }
+        private string SqlitePath { get; set; }
         public DbSet<Location> Locations { get; set; }
-        public DbSet<IServiceRecord> ServiceRecords { get; set; }
+        public DbSet<ServiceRecord> ServiceRecords { get; set; }
 
         #endregion
 
         #region Constructor(s)
         public MobileContext(string sqliteDbPath)
         {
-            databasePath = sqliteDbPath;
+            SqlitePath = sqliteDbPath;
         }
         #endregion
 
         #region Class Methods
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            // use Sqlite Extensions here.
+            optionsBuilder.UseSqlite($"Filename={SqlitePath}");
         }
         #endregion
 
---
+++ src/HvacTools.Mobile/HvacTools.Mobile/Database/MobileReferenceContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HvacTools.Mobile.Database
+{
+    public class MobileReferenceContext
+    {
+
+    }
+}
