d9ab444b781a09a8672bf3c7305e8b1bc1c6a1ff
ANDREWCLAWS6CB1\andrewclawson <andrew.clawson@clawsonanalytics.com>
2017-12-23 13:34:52 -0700

    Add base refrigerants to reference database.

---
+++ etc/SQL/ReferenceDb/Data/refrigerants.sql
@@ -0,0 +1,4 @@
+INSERT INTO refrigerants VALUES (1, 1, 'Dichlorodifluoromethane', 'R-12');
+INSERT INTO refrigerants VALUES (2, 3, 'Chlorodifluoromethane', 'R-22');
+INSERT INTO refrigerants VALUES (3, 5, '1,1,1,2-Tetrafluoroethane', 'R-134a');
+INSERT INTO refrigerants VALUES (4, 5, 'R-32/125 (50+.5,–1.5/50+1.5,–.5)', 'R-410a');
