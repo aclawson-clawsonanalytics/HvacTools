999129d6122658f7b39a508c4221a4bd11c6e1cb
ANDREWCLAWS6CB1\andrewclawson <andrew.clawson@clawsonanalytics.com>
2017-12-23 13:27:11 -0700

    Update refrigerants with foreign key constraints.

--- etc/SQL/ReferenceDb/Tables/refrigerants.sql
+++ etc/SQL/ReferenceDb/Tables/refrigerants.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS 'refrigerants' (
 	'pk_refrigerant_id' INTEGER NOT NULL,
-	'refrigerant_name' TEXT,
-	'refrigerant_code' TEXT,
+	'fk_refrigerant_type_id' INTEGER NOT NULL,
+	'chemical_name' TEXT,
+	'ashrae_code' TEXT,
 	
-	PRIMARY KEY ('pk_refrigerant_id')
+	PRIMARY KEY ('pk_refrigerant_id'),
+	FOREIGN KEY ('fk_refrigerant_type_id') REFERENCES refrigerant_types('pk_refrigerant_type_id')
 )
