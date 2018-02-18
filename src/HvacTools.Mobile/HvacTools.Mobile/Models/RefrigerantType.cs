71d4ced22801f4a4360a87341471aadf2e48421e
ANDREWCLAWS6CB1\andrewclawson <andrew.clawson@clawsonanalytics.com>
2018-02-17 21:32:00 -0700

    Add refrigerent types model.

---
+++ src/HvacTools.Mobile/HvacTools.Mobile/Models/RefrigerantType.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+#region Internal Libraries
+using Tools.Models;
+#endregion
+
+namespace HvacTools.Mobile.Models
+{
+    [Table("refrigerant_types")]
+    public class RefrigerantType
+    {
+        #region Member Variables
+        [Column("pk_refrigerant_type_id")]
+        public int RefrigerantTypeId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("refrigerant_type_code")]
+        public string Code { get; set; }
+        #endregion
+    }
+}
